784bc003e3e9ccff51ab036094c1b456860ebf3d
vietnh <vietnh@vietis.com.vn>
2024-02-22 11:34:20 +0700

    commit 2

--- AppS1mple/SceneDelegate.swift
+++ AppS1mple/SceneDelegate.swift
@@ -17,6 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
       print("commit 1")
+      print("commit 2")
     guard let _ = (scene as? UIWindowScene) else { return }
   }
 
